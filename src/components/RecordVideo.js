b0dce6b8235038955c9cf697204e619a158c3fcf
Disha Chaudhary <dishachaudhary76@gmail.com>
2023-07-18 23:38:38 +0530

    UI Change and Bug Fixes

--- src/components/RecordVideo.js
+++ src/components/RecordVideo.js
@@ -1,6 +1,7 @@
 import React, { useRef, useState, useEffect } from 'react';
 import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import '../styles/RecordVideo.css';
 
 const RecordVideo = () => {
   const videoRef = useRef(null);
@@ -12,21 +13,29 @@ const RecordVideo = () => {
   const [showFaceMetric, setShowFaceMetric] = useState(false);
   const [currentMetricIndex, setCurrentMetricIndex] = useState(0);
   const [capturedImages, setCapturedImages] = useState([]);
+  const [currentToastId, setCurrentToastId] = useState(null);
+  const [isRecordingStarted, setIsRecordingStarted] = useState(false);
 
   const handleNextMetric = () => {
     setCurrentMetricIndex((prevIndex) => {
       const nextIndex = (prevIndex + 1) % faceMetrics.length;
       if (nextIndex === 0) {
         setShowFaceMetric(false);
-        mediaRecorderRef.current.stop(); // Stop recording after all face metrics
-        captureImage(); // Capture an image when all metrics are completed
+        mediaRecorderRef.current.stop();
+        captureImage();
       } else {
+        // Dismiss the previous toast if it exists
+        if (currentToastId) {
+          toast.dismiss(currentToastId);
+        }
+
         // Show toast message for the next face metric
-        toast.info(faceMetrics[nextIndex], {
-          autoClose: 3000,
-          position: toast.POSITION.TOP_CENTER
+        const toastId = toast.info(faceMetrics[nextIndex], {
+          position: toast.POSITION.TOP_CENTER,
+          autoClose: false, // Remove the timer from the toast
         });
-        captureImage(); // Capture an image when moving to the next metric
+        setCurrentToastId(toastId);
+        captureImage();
       }
       return nextIndex;
     });
@@ -42,25 +51,28 @@ const RecordVideo = () => {
     setCapturedImages((prevImages) => [...prevImages, image]);
   };
 
-
   const handleStartRecording = () => {
     setShowFaceMetric(true);
     setCurrentMetricIndex(0);
     chunksRef.current = [];
     setRecordedVideo(null);
     setCapturedImages([]);
-  
+
+    // Remove any existing toasts before starting recording
+    toast.dismiss();
+
     navigator.mediaDevices
       .getUserMedia({ video: true })
       .then((stream) => {
         videoRef.current.srcObject = stream;
         mediaRecorderRef.current = new MediaRecorder(stream);
         setMediaStream(stream);
-  
+
         mediaRecorderRef.current.addEventListener('dataavailable', handleDataAvailable);
         mediaRecorderRef.current.addEventListener('stop', handleSaveVideo);
         mediaRecorderRef.current.start();
         setIsRecording(true);
+        setIsRecordingStarted(true);
       })
       .catch((error) => {
         console.error('Error accessing camera:', error);
@@ -95,44 +107,68 @@ const RecordVideo = () => {
   const faceMetrics = ['Front', 'Up', 'Down', 'Left', 'Right', 'With Mask', 'With Spectacles'];
 
   useEffect(() => {
-    if (isRecording) {
+    if (isRecording && currentMetricIndex === 0) {
       // Show initial toast message for the first face metric
-      toast.info(faceMetrics[currentMetricIndex], {
-        autoClose: 3000,
-        position: toast.POSITION.TOP_CENTER
+      const toastId = toast.info(faceMetrics[currentMetricIndex], {
+        position: toast.POSITION.TOP_CENTER,
+        autoClose: false, // Remove the timer from the toast
       });
+
+      setCurrentToastId(toastId);
+
+      return () => {
+        // Remove the toast when the component unmounts or when the next toast is shown
+        if (currentToastId) {
+          toast.dismiss(currentToastId);
+        }
+      };
     }
-  }, [isRecording]);
+  }, [isRecording, currentMetricIndex]);
 
   return (
-    <div>
-      <video ref={videoRef} autoPlay={true} />
-      {!isRecording ? (
-        <button onClick={handleStartRecording}>
-          {recordedVideo ? 'Re-record' : 'Start Recording'}
-        </button>
-      ) : (
-        <button disabled>Recording...</button>
-      )}
+    <div className="record-video-container">
+      <div className="video-container">
+        <video ref={videoRef} autoPlay={true} className="video-preview" />
+        {!isRecordingStarted ? (
+          <button className="record-button" onClick={handleStartRecording}>
+            {recordedVideo ? 'Re-record' : 'Start Recording'}
+          </button>
+        ) : (
+          <>
+            {isRecording ? (
+              <button className="record-button" disabled>
+                Recording...
+              </button>
+            ) : (
+              <button className="record-button" onClick={handleStartRecording}>
+                Re-record
+              </button>
+            )}
+          </>
+        )}
+      </div>
       {recordedVideo && !isRecording && (
-        <div>
-          <video src={recordedVideo} controls />
-          <button onClick={handleSubmitVideo}>Submit Video</button>
+        <div className="recorded-video-container">
+          <video src={recordedVideo} controls className="recorded-video" />
+          
         </div>
       )}
       {showFaceMetric && (
         <div className="face-metric-card">{faceMetrics[currentMetricIndex]}</div>
       )}
-  
-      <button onClick={handleNextMetric} disabled={!isRecording}>
-        Next Metric
-      </button>
+      {isRecordingStarted && (
+        <button className="next-metric-button" onClick={handleNextMetric} disabled={!isRecording}>
+          Capture
+        </button>
+      )}
       {capturedImages.length > 0 && !isRecording && (
-        <div>
+        <div className="captured-images-container">
           {capturedImages.map((image, index) => (
-            <img key={index} src={image} alt={`Captured ${index + 1}`} />
+            <img key={index} src={image} alt={`Captured ${index + 1}`} className="captured-image" />
           ))}
-          <button onClick={handleSubmitVideo}>Submit Video</button>
+          <button className="submit-button" onClick={handleSubmitVideo}>
+            Submit
+          </button>
         </div>
       )}
       <ToastContainer />
