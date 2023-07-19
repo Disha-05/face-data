2799cc3ba816a7e3bd1beef9fe6d5b8283ddec24
Disha Chaudhary <dishachaudhary76@gmail.com>
2023-07-19 16:05:24 +0530

    Delete irrelevant components

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import UserForm from './components/UserForm';
 import DataCollection from './components/DataCollection';
-import VideoRecorder from './components/VideoRecorder';
-import FaceData from './components/FaceData';
 import RecordVideo from './components/RecordVideo';
 
 const App = () => {
@@ -12,8 +10,6 @@ const App = () => {
       <Routes>
         <Route path="/" element={<UserForm />} />
         <Route path="/data" element={<DataCollection />} />
-        <Route path="/video" element={<VideoRecorder />} />
-        <Route path="/face" element={<FaceData />} />
         <Route path="/rec" element={<RecordVideo />} />
       </Routes>
     </Router>
