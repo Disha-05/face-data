835e86f49d66aef91ad231fd5a30216f4b064af0
Disha Chaudhary <dishachaudhary76@gmail.com>
2023-08-25 23:21:30 +0530

    Crop Image

--- src/components/RecordVideo.js
+++ src/components/RecordVideo.js
@@ -49,15 +49,39 @@ const RecordVideo = () => {
     });
   };
 
-  const captureImage = () => {
+  // const captureImage = () => {
+  //   const canvas = document.createElement('canvas');
+  //   canvas.width = videoRef.current.videoWidth;
+  //   canvas.height = videoRef.current.videoHeight;
+  //   const context = canvas.getContext('2d');
+  //   context.drawImage(videoRef.current, 0, 0, canvas.width, canvas.height);
+  //   const image = canvas.toDataURL('image/png');
+  //   setCapturedImages((prevImages) => [...prevImages, image]);
+  // };
+
+  const captureImage = async () => {
     const canvas = document.createElement('canvas');
     canvas.width = videoRef.current.videoWidth;
     canvas.height = videoRef.current.videoHeight;
     const context = canvas.getContext('2d');
     context.drawImage(videoRef.current, 0, 0, canvas.width, canvas.height);
-    const image = canvas.toDataURL('image/png');
-    setCapturedImages((prevImages) => [...prevImages, image]);
+  
+    // Detect faces on the captured frame
+    const detections = await faceapi.detectAllFaces(canvas, new faceapi.TinyFaceDetectorOptions());
+  
+    // Crop and save the detected face images
+    detections.forEach((detection, index) => {
+      const { x, y, width, height } = detection.box;
+      const faceCanvas = document.createElement('canvas');
+      faceCanvas.width = width;
+      faceCanvas.height = height;
+      const faceContext = faceCanvas.getContext('2d');
+      faceContext.drawImage(canvas, x, y, width, height, 0, 0, width, height);
+      const faceImage = faceCanvas.toDataURL('image/png');
+      setCapturedImages((prevImages) => [...prevImages, faceImage]);
+    });
   };
+  
 
   // const faceMyDetect = async () => {
   //   const canvas = faceapi.createCanvasFromMedia(videoRef.current);
@@ -121,6 +145,47 @@ const RecordVideo = () => {
 //   }
   
 
+  // const handleStartRecording = async () => {
+  //   setShowFaceMetric(true);
+  //   setCurrentMetricIndex(0);
+  //   chunksRef.current = [];
+  //   setRecordedVideo(null);
+  //   setCapturedImages([]);
+  //   setVideoString(null);
+
+  //   navigator.mediaDevices
+  //     .getUserMedia({ video: true })
+  //     .then(async(stream) => {
+  //       videoRef.current.srcObject = stream;
+  //       mediaRecorderRef.current = new MediaRecorder(stream);
+  //       //setMediaStream(stream);
+
+  //       mediaRecorderRef.current.addEventListener('dataavailable', handleDataAvailable);
+  //       mediaRecorderRef.current.addEventListener('stop', handleSaveVideo);
+  //       //mediaRecorderRef.current.start();
+
+  //       // Start face detection
+  // await faceapi.nets.tinyFaceDetector.loadFromUri('/models'); // Load face detection model
+  
+  // const detections = await faceapi.detectAllFaces(videoRef.current, new faceapi.TinyFaceDetectorOptions());
+
+  // if (detections.length > 0) {
+  //   faceMyDetect();
+  //   setIsRecordingStarted(true);
+  //   mediaRecorderRef.current.start();
+  //   setIsRecording(true);
+    
+  // } else {
+  //   console.log('No face detected.');
+  //   // Handle case when no face is detected (e.g., show an error message)
+  //   alert("No face detected");
+  // }
+  //     })
+  //     .catch((error) => {
+  //       console.error('Error accessing camera:', error);
+  //     });
+  // };
+
   const handleStartRecording = async () => {
     setShowFaceMetric(true);
     setCurrentMetricIndex(0);
@@ -128,39 +193,47 @@ const RecordVideo = () => {
     setRecordedVideo(null);
     setCapturedImages([]);
     setVideoString(null);
-
-    navigator.mediaDevices
-      .getUserMedia({ video: true })
-      .then(async(stream) => {
+  
+    try {
+      // Check camera permission
+      const cameraPermission = await navigator.permissions.query({ name: 'camera' });
+  
+      if (cameraPermission.state === 'granted') {
+        const stream = await navigator.mediaDevices.getUserMedia({ video: true });
         videoRef.current.srcObject = stream;
         mediaRecorderRef.current = new MediaRecorder(stream);
-        //setMediaStream(stream);
-
+        
         mediaRecorderRef.current.addEventListener('dataavailable', handleDataAvailable);
         mediaRecorderRef.current.addEventListener('stop', handleSaveVideo);
-        //mediaRecorderRef.current.start();
-
-        // Start face detection
-  await faceapi.nets.tinyFaceDetector.loadFromUri('/models'); // Load face detection model
   
-  const detections = await faceapi.detectAllFaces(videoRef.current, new faceapi.TinyFaceDetectorOptions());
-
-  if (detections.length > 0) {
-    faceMyDetect();
-    setIsRecordingStarted(true);
-    mediaRecorderRef.current.start();
-    setIsRecording(true);
-    
-  } else {
-    console.log('No face detected.');
-    // Handle case when no face is detected (e.g., show an error message)
-    alert("No face detected");
-  }
-      })
-      .catch((error) => {
-        console.error('Error accessing camera:', error);
-      });
+        // Load the face detection model
+        await faceapi.nets.tinyFaceDetector.loadFromUri('/models');
+        
+        // Detect faces
+        const detections = await faceapi.detectAllFaces(videoRef.current, new faceapi.TinyFaceDetectorOptions());
+  
+        if (detections.length > 0) {
+          faceMyDetect();
+          setIsRecordingStarted(true);
+          mediaRecorderRef.current.start();
+          setIsRecording(true);
+        } else {
+          console.log('No face detected.');
+          // Handle case when no face is detected (e.g., show an error message)
+          alert('No face detected');
+        }
+      } else if (cameraPermission.state === 'prompt') {
+        // The user might be prompted to grant permission
+        console.log('Camera permission prompt');
+      } else {
+        // Permission denied
+        console.error('Camera permission denied');
+      }
+    } catch (error) {
+      console.error('Error accessing camera:', error);
+    }
   };
+  
 
   const handleDataAvailable = (event) => {
     if (event.data.size > 0) {
